909688ee35dc3f1710b99a22726091f08df5ad52
missyolin eunike <101983657+Missyolin@users.noreply.github.com>
2024-03-18 16:34:47 +0700

    Perubahan 1
    Menambahkan activity

---
+++ app/src/main/java/com/ifs21008/dinopedia/DinoSpecification.kt
@@ -0,0 +1,62 @@
+package com.ifs21008.dinopedia
+
+import android.R
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import com.ifs21008.dinopedia.databinding.ActivityDinoSpecificationBinding
+
+class DinoSpecification : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDinoSpecificationBinding
+    private var dino: Dinosaurus? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDinoSpecificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        dino = if (Build.VERSION.SDK_INT >=33) {
+            intent.getParcelableExtra(EXTRA_ITEM, Dinosaurus::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra(EXTRA_ITEM)
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        if (dino != null) {
+            supportActionBar?.title = "Item ${dino!!.dino_name}"
+            loadData(dino!!)
+        } else{
+            finish()
+        }
+    }
+
+    private fun loadData(dino: Dinosaurus) {
+        binding.ivDinoSpec.setImageResource(dino.dino_icon)
+        binding.tvDinoName.text = dino.dino_name
+        binding.tvDinoDesc.text = dino.dino_desc
+        binding.tvDinoLife.text = dino.dino_period_time
+        binding.tvDinoChar.text = dino.dino_physics_characteristic
+        binding.tvdinoHab.text = dino.dino_habit
+        binding.tvDinoFood.text = dino.dino_food
+        binding.tvDinoLeng.text = dino.dino_length
+        binding.tvDinoHei.text = dino.dino_height
+        binding.tvDinoWe.text = dino.dino_weight
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.home -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        const val EXTRA_ITEM = "extra_item"
+    }
+
+
+}
